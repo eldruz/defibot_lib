bb1ea66663a2981210410c11e5377d17046f16ca
Andrés Saraos Luna <eldruz@protonmail.com>
2017-08-05 14:03:39 +0200

    Changes the mock implementation according to the changes in the model and adds new persistence traits.

--- examples/mockdata.rs
+++ examples/mockdata.rs
@@ -1,10 +1,6 @@
 extern crate defibot;
 
 use defibot::model::{
-    PlayerId,
-    GameId,
-    FtId,
-    FtState,
     Player,
     Game,
     Ft,
@@ -18,26 +14,50 @@ pub struct MockData {
     pub results: Vec<Ft>,
     pub players: Vec<Player>,
     pub games: Vec<Game>,
-    pub next_id: u8,
+    pub next_id: usize,
 }
 
 impl MockData {
     fn new() -> MockData {
-        let default_game = Game {id: GameId(0), name: String::from("BouleFighter")};
-        let default_pone = Player {id: PlayerId(0), nick: String::from("eldruz")};
-        let default_ptwo = Player {id: PlayerId(1), nick: String::from("quinonino")};
+        let default_game = Game {name: String::from("BouleFighter")};
+        let default_pone = Player {nick: String::from("eldruz")};
+        let default_ptwo = Player {nick: String::from("quinonino")};
         MockData { results: vec![], players: vec![default_pone, default_ptwo], games: vec![default_game], next_id: 0}
     }
 }
 
 impl ResultsPersistence for MockData {
-    fn get_ft(&mut self, ft: FtId) -> Option<&mut Ft> {
+    fn get_ft(&mut self, ft: usize) -> Option<&mut Ft> {
         self.results.iter_mut().find(|x| x.id == ft)
     }
 
-    fn register_ft(&mut self, game: GameId, player_a: PlayerId, player_b: PlayerId, score_a: u8, score_b: u8) {
-        let id = FtId(self.next_id);
-        let ft = Ft::new(id, game, player_a, player_b, score_a, score_b);
+    fn get_player(&mut self, p_id: &str) -> Option<&mut Player> {
+        self.players.iter_mut().find(|x| x.nick == p_id)
+    }
+
+    fn get_game(&mut self, g_id: &str) -> Option<&mut Game> {
+        self.games.iter_mut().find(|x| x.name == g_id)
+    }
+
+    fn add_player(&mut self, player: &str) {
+        match self.players.iter().find(|x| x.nick == player) {
+            None => self.players.push(Player {nick: player.to_string()}),
+            Some(_) => ()
+        };
+    }
+
+    fn add_game(&mut self, game: &str) {
+        match self.games.iter().find(|x| x.name == game) {
+            None => self.games.push(Game{name: game.to_string()}),
+            Some(_) => ()
+        };
+    }
+
+    fn register_ft(&mut self, game: &str, player_a: &str, player_b: &str, score_a: u8, score_b: u8) {
+        let id = self.next_id;
+        let ft = Ft::new(id, Game{name: game.to_string()}, Player{nick: player_a.to_string()}, Player{nick: player_b.to_string()}, score_a, score_b);
+        self.add_player(player_a);
+        self.add_player(player_b);
         self.results.push(ft);
         self.next_id += 1;
     }
@@ -48,10 +68,14 @@ impl ResultsRules for MockData {}
 fn main() {
     let mut mock_data = MockData::new();
 
-    mock_data.register_ft(GameId(0), PlayerId(1), PlayerId(0), 5, 4);
+    mock_data.register_ft("BouleFighter", "el poncho", "quinonino", 5, 4);
     {
-        let result = mock_data.get_ft(FtId(0)).expect("AIEAIEAIE");
-        match MockData::validate_ft(result, PlayerId(0), true) {
+        let result = mock_data.get_ft(0).expect("AIEAIEAIE");
+        match MockData::validate_ft(result, "eldruz", true) {
+            Err(e) => println!("ERROR : {}", e),
+            Ok(_) => println!("Success")
+        }
+        match MockData::validate_ft(result, "quinonino", true) {
             Err(e) => println!("ERROR : {}", e),
             Ok(_) => println!("Success")
         }
