2a41f742850a210b71aed2f346487b1593b81733
Andrés Saraos Luna <eldruz@protonmail.com>
2017-08-05 14:02:52 +0200

    Simplifies the id models

--- src/model.rs
+++ src/model.rs
@@ -1,18 +1,3 @@
-macro_rules! id {
-	($($name:ident;)*) => {
-		$(
-			#[derive(Copy, Clone, Hash, Eq, PartialEq, Debug, Ord, PartialOrd)]
-			pub struct $name(pub u8);
-		)*
-	}
-}
-
-id! {
-	PlayerId;
-    GameId;
-    FtId;
-}
-
 #[derive(Debug, PartialEq)]
 pub enum FtState {
     // Pending(PlayerId)
@@ -21,31 +6,29 @@ pub enum FtState {
     Confirmed,
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct Player {
-    pub id: PlayerId,
     pub nick: String,
 }
 
 #[derive(Debug)]
 pub struct Game {
-    pub id: GameId,
     pub name: String,
 }
 
 #[derive(Debug)]
 pub struct Ft {
-    pub id: FtId,
-    pub game: GameId,
-    pub player_a: PlayerId,
-    pub player_b: PlayerId,
+    pub id: usize,
+    pub game: Game,
+    pub player_a: Player,
+    pub player_b: Player,
     pub score_a: u8,
     pub score_b: u8,
     pub state: FtState,
 }
 
 impl Ft {
-    pub fn new(id: FtId, game: GameId, player_a: PlayerId, player_b: PlayerId, score_a: u8, score_b: u8) -> Ft {
+    pub fn new(id: usize, game: Game, player_a: Player, player_b: Player, score_a: u8, score_b: u8) -> Ft {
         // for now we assume that player_a is the initiator
         Ft {
             id,
--- src/rules.rs
+++ src/rules.rs
@@ -1,13 +1,18 @@
 use model::*;
 
 pub trait ResultsPersistence {
-    fn get_ft(&mut self, ft: FtId) -> Option<&mut Ft>;
-    fn register_ft(&mut self, game: GameId, player_a: PlayerId, player_b: PlayerId, score_a: u8, score_b: u8);
+    fn get_player(&mut self, p_id: &str) -> Option<&mut Player>;
+    fn get_game(&mut self, g_id: &str) -> Option<&mut Game>;
+    fn get_ft(&mut self, ft: usize) -> Option<&mut Ft>;
+
+    fn register_ft(&mut self, game: &str, player_a: &str, player_b: &str, score_a: u8, score_b: u8);
+    fn add_player(&mut self, player: &str);
+    fn add_game(&mut self, game: &str);
 }
 
 pub trait ResultsRules {
-    fn validate_ft(ft: &mut Ft, player: PlayerId, confirm: bool) -> Result<&Ft, &'static str> {
-        if ft.player_b == player && ft.state == FtState::Pending {
+    fn validate_ft<'a, 'b>(ft: &'a mut Ft, player: &'b str, confirm: bool) -> Result<&'a Ft, &'static str> {
+        if ft.player_b.nick == player && ft.state == FtState::Pending {
             // change the state depending of the confirm
             match confirm {
                 false => ft.state = FtState::Canceled,
